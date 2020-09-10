767de2b62b6d63ccdfcf0537fa1b29f75da02790
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-09-10 11:36:49 -0300

    aux1

--- primefact.c
+++ primefact.c
@@ -5,19 +5,25 @@ void factoresPrimos(int n){
     int p=2;
     while (n>=p*p){
         if (n%p==0){
-            printf("%d", p);
+            printf("%d  ", p);
             n=n/p;
         }
         else {
             p++;
         }
     }
+    printf("%d",n);
     return;
 }
 
 int main(int argc, char *argv[]){
     int n =atoi(argv[1]);
-    factoresPrimos(n);
-    printf("/n");
+    if (n==1){
+        printf(1);
+    }
+    else{
+        factoresPrimos(n);
+    }
+    printf("\n");
     return 0;
     }
