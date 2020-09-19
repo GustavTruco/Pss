fb237864632118aedc9d0a7c75f64ac31ea80f19
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-09-19 00:07:32 -0300

    tarea 1 v1

--- T1/t1.c
+++ T1/t1.c
@@ -4,6 +4,19 @@
 #include "t1.h"
 
 unsigned long long dupMasLargo(unsigned long long x){
-    int tam;
-    
+    unsigned long long tam = 0;
+    unsigned int cadena = 0;
+    for (unsigned int i=1;i<=32;i++){
+      	if (x>>i != 0){
+            unsigned int mask= (1<<i)-1;
+            unsigned int p_cadena= x & mask;
+            if ((p_cadena ^ ((x >> i) & mask))==0){
+                tam=i;
+                cadena=p_cadena;
+            }
+        }
+    }
+
+    unsigned long long result = (tam << 32)|cadena;
+    return result;
 }
