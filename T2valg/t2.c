2bd12ff258209fd2ef4de9b4064e311f8539165f
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-10-06 23:42:46 -0300

    tarea 2

---
+++ T2valg/t2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void reducir(char *s){
+    char *p=s;
+	char *r=s;
+	char *q=s+1;
+	int i=0;
+	while (*p){
+	    if (*r==' ' && *q==' '){
+	        r++;
+	        q++;
+	        *p=*r;
+	    }
+	    else{
+	       	*p=*r;
+	        p++;
+	        r++;
+	        q++; 
+	    }
+	}
+}
+
+char *reduccion(char *st){
+    char *p=st;
+	char *t=st+1;
+	int len=1;
+	while(*p){
+	    if(*p!=' ' || *t!=' '){
+	        len++;
+	    }
+	    p++;
+	    t++;
+	}
+	char *w=malloc(len*sizeof(char));
+	char *r=st;
+	char *y=st+1;
+	while(*r){
+	    if(*r!=' ' || *y!=' '){
+	        *w=*r;
+	    }
+	    printf("%c",*w);
+	    r++;
+	    w++;
+	    y++;
+	}
+	return w;
+}
