cd363a4088e9fa3fe6f798842d326af83739ea0f
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-10-27 00:24:52 -0300

    tarea 3

---
+++ T3/t3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "t3.h";
+
+void desbalancear(Nodo **pa, Nodo **pult){
+    Nodo *nodo_actual=*pa;
+    Nodo *nodo_pas= *pult;
+    if (nodo_actual->der==NULL && nodo_actual->izq==NULL){
+        nodo_actual->der=nodo_pas;
+        return;
+    }
+    else if (nodo_actual->der==NULL && nodo_pas!=NULL){
+        nodo_pas->izq=nodo_actual->izq;
+        nodo_actual->izq=NULL;
+        desbalancear(& nodo_actual->izq,& nodo_actual);
+    }
+   
+    desbalancear(& nodo_actual->izq,& nodo_actual);
+    desbalancear(& nodo_actual->der,& *NULL);
+
+    Nodo *paux= nodo_actual->izq;
+
+    while(paux->der!=NULL){
+        paux=paux->der;
+    }
+    paux->der=nodo_actual;
+    nodo_actual->izq=NULL;
+}
