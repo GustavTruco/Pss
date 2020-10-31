6202abafd54f090d9994a157d0f845bdc0715431
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-10-30 22:22:20 -0300

    segundo intento tarea 3

--- T3/t3.c
+++ T3/t3.c
@@ -7,7 +7,7 @@ void desbalancear(Nodo **pa, Nodo **pult){
     if (*pa==NULL){
         return;
     }
-    printf("Nodo Id= %d", pa->id);
+    printf("Nodo Id= %d", *pa->id);
 
     Nodo *nodo_actual=*pa;
     if (nodo_actual->izq==NULL && nodo_actual->der==NULL){
@@ -21,7 +21,7 @@ void desbalancear(Nodo **pa, Nodo **pult){
         while (aux->der!=NULL){
             aux=aux->der;
         }
-        **pa=*(nodo_actual->izq);
+        **pa=&(nodo_actual->izq);
         nodo_actual->izq=NULL;
         aux->der=nodo_actual;
     }
