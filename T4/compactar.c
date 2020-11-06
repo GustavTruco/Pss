02c3ade33fdc33ad0cd0404c633f77b175dbc0d8
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-11-05 23:46:28 -0300

    Tarea 4 segundo intento

--- T4/compactar.c
+++ T4/compactar.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
     FILE * archivo;
     char buffer[20];
     char blank[20]="                   \n";
-    if ( (archivo =fopen(argv[1],"wr"))==NULL){
+    if ( (archivo =fopen(argv[1],"r+"))==NULL){
         perror(argv[1]);
         exit(1);
     }
@@ -31,4 +31,5 @@ int main(int argc, char *argv[]){
         fwrite(&blank,sizeof(char),20,archivo);
         j++;
     }
+    fclose(archivo);
 }
