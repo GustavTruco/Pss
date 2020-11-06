ba79b71103f90a9e080e0ba1a2cc062fcec1963e
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-11-05 23:23:37 -0300

    Tarea 4 primer intento

---
+++ T4/compactar.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+    FILE * archivo;
+    char buffer[20];
+    char blank[20]="                   \n";
+    if ( (archivo =fopen(argv[1],"wr"))==NULL){
+        perror(argv[1]);
+        exit(1);
+    }
+    int i=0,j=0;
+    fseek(archivo,i*20,SEEK_SET);
+    int count=fread(&buffer, sizeof(char), 20, archivo);
+    buffer[20]=0;
+    while (count!=0){
+    if (strncmp(buffer,blank,19)!=0){
+        fseek(archivo,20*j,SEEK_SET);
+        fwrite(&buffer,sizeof(char),20,archivo);
+        j++;
+    }
+    i++;
+    fseek(archivo,i*20,SEEK_SET);
+    count = fread(&buffer, sizeof(char), 20, archivo);
+    buffer[20]=0;
+    }
+    while (i>j){
+        fseek(archivo,20*j,SEEK_SET);
+        fwrite(&blank,sizeof(char),20,archivo);
+        j++;
+    }
+}
