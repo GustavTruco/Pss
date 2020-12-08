0a3db6984c6363a44697c88bcf2e8009e25f07fa
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-12-08 12:07:42 -0300

    Tarea 6

--- T6/t6.c
+++ T6/t6.c
@@ -7,19 +7,22 @@
 pthread_mutex_t m= PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond= PTHREAD_COND_INITIALIZER;
 
-int visor= 0;
-int num=0;
+int visor;
+int num;
 
 void ocupar() {
   pthread_mutex_lock(&m);
   int mi_num=num;
+  num++;
   while(visor!=mi_num){
     pthread_cond_wait(&cond,&m);
   }
-  num++;
+  pthread_mutex_unlock(&m);
+  
 }
 
 void desocupar() {
+  pthread_mutex_lock(&m);
   visor++;
   pthread_cond_broadcast(&cond);
   pthread_mutex_unlock(&m);
